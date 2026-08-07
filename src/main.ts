d76c16b89bf029d73f8ad252cb4c5481daae24db
Anurag Yadav <anuragyadav622003@gmail.com>
2026-08-07 18:58:59 +0530

    pr created

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
-
+import https from 'https';
+https.globalAgent.options.family = 4; // ← force IPv4 for all outbound HTTPS calls
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // capture raw request body for webhook signature verification
